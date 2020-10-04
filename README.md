86c496cbef0d41c507fd73e0ceacd80fbfd7726a
fafa <fafa@debian>
2020-10-05 00:56:33 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # opencv_make
+
+This is the shell script to make libraries of OpenCV-4.4.0 using g++.
+
+## How to use
+
+```
+git clone https://github.com/ya-mat/lapack_etc_make.git
+cd lapack_etc_make
+sh make.sh
+```
+
+## Dependency
+
+You must have already installed these softwares below.
+
+- libgtk2.0-dev
+- pkg-config
+- libavcodec-dev
+- libavformat-dev
+- libswscale-dev
+- Eigen
---
+++ make.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# before
+#sudo apt install gcc
+#sudo apt install g++
+#sudo apt install gfortran
+#sudo apt install wget
+#sudo apt install make
+#sudo apt install cmake
+
+#install Eigen 3.3.7
+wget https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz && \
+tar zxvf eigen-3.3.7.tar.gz && \
+cp -r eigen-3.3.7/Eigen ./
+
+# OpenCV-4.4.0
+wget https://github.com/opencv/opencv/archive/4.4.0.zip && \
+unzip 4.4.0.zip && \
+cd opencv-4.4.0/ && \
+mkdir build && \
+cd build && \
+wget https://github.com/opencv/opencv_contrib/archive/4.4.0.zip && \
+unzip 4.4.0.zip && \
+cmake -D BUILD_JPEG=ON \
+      -D BUILD_PNG=ON \
+      -D BUILD_SHARED_LIBS=OFF \
+      -D BUILD_TIFF=ON \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_INSTALL_PREFIX=./ \
+      -D OPENCV_EXTRA_MODULES_PATH=./opencv_contrib-4.4.0/modules \
+      -D WITH_OPENMP=ON \
+      .. && \
+make -j $(nproc) && \
+make install && \
+cd ../../ && \
+cp -r opencv-4.4.0/build/lib/ ./ && \
+mkdir include && \
+cp -r opencv-4.4.0/build/include/opencv4 ./include/
