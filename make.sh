7c60d77211cb8561cd171e8d4a1ef360c277a5fa
fafa <fafa@debian>
2020-10-07 23:14:01 +0900

    Eigen is omitted

--- make.sh
+++ make.sh
@@ -8,10 +8,11 @@
 #sudo apt install make
 #sudo apt install cmake
 
-#install Eigen 3.3.7
-wget https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz && \
-tar zxvf eigen-3.3.7.tar.gz && \
-cp -r eigen-3.3.7/Eigen ./
+# install Eigen 3.3.7
+# Eigen (and unsupported) is installed to '/usr/local/include'
+#wget https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz && \
+#tar zxvf eigen-3.3.7.tar.gz && \
+#cp -r eigen-3.3.7/Eigen ./
 
 # OpenCV-4.4.0
 wget https://github.com/opencv/opencv/archive/4.4.0.zip && \
